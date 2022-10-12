a0db7afc270f9a076e16c0b313824ac7e18154f3
rodrigogregorioneri <rodrigogregorioneri@hotmail.com>
2022-10-12 06:01:21 -0300

    adicionando mock do event e adicionando localstack para teste local

--- handler.py
+++ handler.py
@@ -2,8 +2,11 @@ import json
 import boto3
 
 def lambdafunction(event, context):
+    print(event)
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = event['Records'][0]['s3']['object']['key']
     s3 = boto3.client("s3")
-    obj = s3.get_object(Bucket="",Key="")
+    obj = s3.get_object(Bucket=bucket,Key=key)
     total_sum, row_count = 0, 0
     for line in obj['Body'].iter_lines(chunk_size=1024, keepsends=False):
         total_sum += int(line(0))
